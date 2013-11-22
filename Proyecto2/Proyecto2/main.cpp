26486781eca99b2b2ca7311763cfeebfb2c32940
Isa Jilo <isajilo7@gmail.com>
2013-11-22 15:13:55 -0430

    Base

---
+++ Proyecto2/Proyecto2/main.cpp
@@ -0,0 +1,156 @@
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+using namespace std;
+
+#define DEF_floorGridScale	1.0
+#define DEF_floorGridXSteps	10.0
+#define DEF_floorGridZSteps	10.0
+
+
+void ejesCoordenada() {
+	
+	glLineWidth(2.5);
+	glBegin(GL_LINES);
+		glColor3f(1.0,0.0,0.0);
+		glVertex2f(0,10);
+		glVertex2f(0,-10);
+		glColor3f(0.0,0.0,1.0);
+		glVertex2f(10,0);
+		glVertex2f(-10,0);
+	glEnd();
+
+	glLineWidth(1.5);
+	int i;
+	glColor3f(0.0,1.0,0.0);
+	glBegin(GL_LINES);
+		for(i = -10; i <=10; i++){
+			if (i!=0) {		
+				if ((i%2)==0){	
+					glVertex2f(i,0.4);
+					glVertex2f(i,-0.4);
+
+					glVertex2f(0.4,i);
+					glVertex2f(-0.4,i);
+				}else{
+					glVertex2f(i,0.2);
+					glVertex2f(i,-0.2);
+
+					glVertex2f(0.2,i);
+					glVertex2f(-0.2,i);
+
+				}
+			}
+		}
+		
+	glEnd();
+
+	glLineWidth(1.0);
+}
+
+
+void changeViewport(int w, int h) {
+	
+	float aspectratio;
+
+	if (h==0)
+		h=1;
+
+	
+	glViewport(0,0,w,h);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	
+	aspectratio = (float) w / (float) h;
+
+	if (w<=h) {
+		
+		glOrtho(-10.0,10.0,-10.0/aspectratio,10.0/aspectratio,1.0,50.0);
+
+	}else {
+
+		glOrtho(-10.0*aspectratio,10.0*aspectratio,-10.0,10.0,1.0,50.0);
+
+	}
+
+
+	glMatrixMode (GL_MODELVIEW);
+}
+
+void render(){
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	GLfloat zExtent, xExtent, xLocal, zLocal;
+    int loopX, loopZ;
+
+	glLoadIdentity ();                       
+	gluLookAt (0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	
+	
+	// Render Grid 
+	
+	glLineWidth(1.0);
+	glPushMatrix();
+    glColor3f( 0.0, 0.7, 0.7 );
+    glBegin( GL_LINES );
+    zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+    for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+	{
+	xLocal = DEF_floorGridScale * loopX;
+	glVertex3f( xLocal, -zExtent, 0.0 );
+	glVertex3f( xLocal, zExtent,  0.0 );
+	}
+    xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+	{
+	zLocal = DEF_floorGridScale * loopZ;
+	glVertex3f( -xExtent, zLocal, 0.0 );
+	glVertex3f(  xExtent, zLocal, 0.0 );
+	}
+    glEnd();
+    glPopMatrix();
+	
+	// Fin Grid
+
+
+	//Suaviza las lineas
+	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_LINE_SMOOTH );
+
+	
+	ejesCoordenada();
+
+
+	glDisable(GL_BLEND);
+	glDisable(GL_LINE_SMOOTH);
+
+	glutSwapBuffers();
+}
+
+int main (int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+	glutInitWindowSize(800,800);
+
+	glutCreateWindow("Test Opengl");
+
+	glutReshapeFunc(changeViewport);
+	glutDisplayFunc(render);
+
+	
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+	
+
+	glutMainLoop();
+	return 0;
+
+}
